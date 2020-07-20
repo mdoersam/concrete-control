2860df37ea168f928deda41fd3c97cd14adea42b
Michael Dörsam <m.doersam@mailbox.org>
2020-07-20 17:05:41 +0200

    Use incremental encoder and lower resolution

--- Concrete_control.ino
+++ Concrete_control.ino
@@ -5,14 +5,16 @@
 #include "ESPRotary.h";
 
 
-#define ROTARY_PIN1  14
-#define ROTARY_PIN2 22
+#define ROTARY_PIN1 22
+#define ROTARY_PIN2 14
 #define BUTTON_PIN  23
 
-#define CLICKS_PER_STEP   4  
+#define CLICKS_PER_STEP   1
+
+const int encoderResolution = 5;
 
 
-ESPRotary r = ESPRotary(ROTARY_PIN1, ROTARY_PIN2, CLICKS_PER_STEP);
+ESPRotary r = ESPRotary(ROTARY_PIN1, ROTARY_PIN2);//, CLICKS_PER_STEP);
 Button2 b = Button2(BUTTON_PIN);
 
 BleKeyboard bleKeyboard = BleKeyboard("Concrete Control", "md-tech");
@@ -30,24 +32,35 @@ void setup() {
   b.setLongClickHandler(resetPosition);
 }
 
+
+int previous1 = 0;
+int previous2 = 0;
+
 void loop() {
   r.loop();
   b.loop();
 }
 
 void rotate(ESPRotary& r) {
-    Serial.println(r.getPosition());
+   // Serial.println(r.getPosition());
 }
 
+int encoderActionCount = 0;
 
 void showDirection(ESPRotary& r) {
+  if((++encoderActionCount % encoderResolution) == 0){
+    
     if (r.getDirection() == RE_RIGHT) {
+      Serial.println("up");
       bleKeyboard.write(KEY_MEDIA_VOLUME_UP);
     } else {
+      Serial.println("down");
       bleKeyboard.write(KEY_MEDIA_VOLUME_DOWN);
     }
     
     bleKeyboard.releaseAll();
+    
+  }
 }
 
 void click(Button2& btn) {
